8579d5b46928ce1b8c5c8e1056a479ad1d51aee5
Darwinpz <dpilaloa1@gmail.com>
2021-04-07 18:04:42 -0500

    Update index.js

--- backend-sisport/src/routes/index.js
+++ backend-sisport/src/routes/index.js
@@ -68,6 +68,7 @@ module.exports = (app) => {
     //RUTAS DE LAS PERSONAS
     app.get('/api/persona', persona.all);
     app.get('/api/persona/:id', persona.find);
+    app.post('/api/persona/login', persona.login);
     app.post('/api/persona/add', persona.add);
     app.put('/api/persona/put', persona.put);
     app.delete('/api/persona/delete', persona.delete);
