ccc7270c14a6ff99c5d4126695ce5ff8d2d3eb4e
Darwinpz <dpilaloa1@gmail.com>
2021-04-21 12:29:48 -0500

    update delete archivos
    borrar archivos de mongodb

--- frontend-sisport/src/components/Modals/archivos.js
+++ frontend-sisport/src/components/Modals/archivos.js
@@ -57,9 +57,17 @@ export default function Archivos() {
         var per_cedula = document.getElementById("per_cedula").innerText
         var tipo_archivo = document.getElementById("tipo_archivo").innerText
         var nombre_archivo = document.getElementById("archivo_nombre").innerText
+        var est_cedula = document.getElementById("est_cedula").innerText
 
 
-        descargarArchivo({fac_abreviatura,car_abreviatura,asig_abreviatura:identificador+"-"+peri_codigo,per_cedula,tipo_archivo,nombre_archivo}).then((url)=>{
+        var cedula = per_cedula
+
+        if(est_cedula){
+
+            cedula = est_cedula
+        }
+
+        descargarArchivo({fac_abreviatura,car_abreviatura,asig_abreviatura:identificador+"-"+peri_codigo,per_cedula:cedula,tipo_archivo,nombre_archivo}).then((url)=>{
 
             var win = window.open(url, '_blank');
             win.focus();
