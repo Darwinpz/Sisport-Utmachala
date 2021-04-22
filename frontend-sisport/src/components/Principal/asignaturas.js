e85e0a0c9ae00b962be21a4f284e81c7b0a3499d
Darwinpz <dpilaloa1@gmail.com>
2021-04-22 00:34:18 -0500

    Update asignaturas.js

--- frontend-sisport/src/components/Principal/asignaturas.js
+++ frontend-sisport/src/components/Principal/asignaturas.js
@@ -1,5 +1,5 @@
 
-import React, {  useState } from "react";
+import React, { useState } from "react";
 
 import usePerfil from 'hooks/usePerfil'
 
@@ -9,40 +9,34 @@ import portafolioPythonService from 'services/python/portafolio'
 export default function Asignaturas({ asignaturas }) {
 
 
-    const [clave, setCLAVE] = useState("");
-
     const [error, setError] = useState("")
 
     const { perfil } = usePerfil()
 
     const jwt = window.localStorage.getItem("jwt")
 
-    const {matricularse,eliminarPortafolio} = portafolioService({jwt})
+    const { matricularse, eliminarPortafolio } = portafolioService({ jwt })
 
-    const handleClick = () => {
+    const handleClick = (asig_codigo, peri_codigo, fac_nombre, car_nombre, asig_identificador) => {
 
-        const asig_codigo = document.getElementById("asig_codigo").value
-        const peri_codigo = document.getElementById("peri_codigo").value
-        const fac_nombre = document.getElementById("fac_nombre").value
-        const car_nombre = document.getElementById("car_nombre").value
-        const asig_identificador = document.getElementById("asig_identificador").value
+        var clave = document.getElementById("clave_" + asig_codigo).value
 
-        const {crearPortafolio} = portafolioPythonService()
+        const { crearPortafolio } = portafolioPythonService()
 
-        matricularse({ asig_codigo, peri_codigo, clave})
+        matricularse({ asig_codigo, peri_codigo, clave })
             .then(() => {
 
-                crearPortafolio({fac_nombre,car_nombre,asig_identificador:asig_identificador+"-"+peri_codigo,per_cedula:perfil.per_cedula}).then(()=>{
+                crearPortafolio({ fac_nombre, car_nombre, asig_identificador: asig_identificador + "-" + peri_codigo, per_cedula: perfil.per_cedula }).then(() => {
 
                     window.location.reload()
 
-                }).catch(()=>{
+                }).catch(() => {
 
-                    eliminarPortafolio({asig_codigo,peri_codigo,per_codigo:perfil.per_codigo}).then(()=>{
+                    eliminarPortafolio({ asig_codigo, peri_codigo, per_codigo: perfil.per_codigo }).then(() => {
 
 
-                    }).catch(()=>{
-                    
+                    }).catch(() => {
+
                         setError("No se eliminar portafolio,  contacte con el coordinador o intente de nuevo")
 
                     })
@@ -55,11 +49,7 @@ export default function Asignaturas({ asignaturas }) {
             })
             .catch(() => {
 
-                console.log("clave incorrecta")
-                
                 setError(asig_codigo)
-
-                print(asig_codigo)
 
             })
 
@@ -71,7 +61,7 @@ export default function Asignaturas({ asignaturas }) {
 
             <div className="row align-items-center">
                 {
-                    asignaturas.map(({ asig_codigo, asig_nombre, sem_nombre, sem_paralelo,asig_identificador, peri_nombre, peri_codigo, docente, matriculado, estado, fac_abreviatura, car_abreviatura }) =>
+                    asignaturas.map(({ asig_codigo, asig_nombre, sem_nombre, sem_paralelo, asig_identificador, peri_nombre, peri_codigo, docente, matriculado, estado, fac_abreviatura, car_abreviatura }) =>
                         <div className="col-md-6 col-lg-4 mb-3" key={asig_codigo}>
 
                             <div className="card border-primary">
@@ -90,14 +80,9 @@ export default function Asignaturas({ asignaturas }) {
                                                 estado &&
                                                 <>
                                                     <div className="d-flex justify-content-between align-items-center">
-                                                        <input style={{ display: "none" }} type="number" id="asig_codigo" className="form-control mr-2" onChange={() => asig_codigo} placeholder="asig_codigo" required value={asig_codigo} />
-                                                        <input style={{ display: "none" }} type="number" id="peri_codigo" className="form-control mr-2" onChange={() => peri_codigo} placeholder="peri_codigo" required value={peri_codigo} />
-                                                        <input style={{ display: "none" }} type="text" id="fac_nombre" className="form-control mr-2" onChange={() => fac_abreviatura} placeholder="fac_nombre" required value={fac_abreviatura} />
-                                                        <input style={{ display: "none" }} type="text" id="car_nombre" className="form-control mr-2" onChange={() => car_abreviatura} placeholder="car_nombre" required value={car_abreviatura} />
-                                                        <input style={{ display: "none" }} type="text" id="asig_identificador" className="form-control mr-2" onChange={() => asig_identificador} placeholder="asig_identificador" required value={asig_identificador} />
-                                                        
-                                                        <input type="text" className="form-control mr-2" onChange={(e) => setCLAVE(e.target.value)} placeholder="Clave" required />
-                                                        <button className="btn btn-primary" type="button" onClick={() => handleClick()}>Matricularme</button>
+
+                                                        <input type="text" className="form-control mr-2" id={`clave_${asig_codigo}`} placeholder="Clave" required />
+                                                        <button className="btn btn-primary" type="button" onClick={() => handleClick(asig_codigo, peri_codigo, fac_abreviatura, car_abreviatura, asig_identificador)}>Matricularme</button>
 
                                                     </div>
                                                     {error === asig_codigo && <strong>Clave incorrecta</strong>}
