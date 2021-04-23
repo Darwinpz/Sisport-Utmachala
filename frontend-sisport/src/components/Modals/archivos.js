9cdd94d243da8502f9026b685078defd95767af8
Darwinpz <dpilaloa1@gmail.com>
2021-04-23 07:42:59 -0500

    update upload

--- frontend-sisport/src/components/Modals/archivos.js
+++ frontend-sisport/src/components/Modals/archivos.js
@@ -7,7 +7,7 @@ import portafolioService from 'services/portafolio'
 export default function Archivos() {
 
 
-    const {descargarArchivo,eliminarArchivo} = archivosPythonService({})
+    const { descargarArchivo, eliminarArchivo } = archivosPythonService({})
 
     const [error, setError] = useState("");
 
@@ -21,36 +21,37 @@ export default function Archivos() {
         var tipo_archivo = document.getElementById("tipo_archivo").innerText
         var nombre_archivo = document.getElementById("archivo_nombre").innerText
         var asig_codigo = document.getElementById("asig_codigo").innerText
+        var sem_codigo = document.getElementById("sem_codigo").innerText
 
         const jwt = localStorage.getItem("jwt")
 
-        const {removefiles} = portafolioService({jwt})
+        const { removefiles } = portafolioService({ jwt })
 
         setError("")
 
-        eliminarArchivo({fac_abreviatura,car_abreviatura,asig_abreviatura:identificador+"-"+peri_codigo,per_cedula,tipo_archivo,nombre_archivo}).then(()=>{
+        eliminarArchivo({ fac_abreviatura, car_abreviatura, asig_abreviatura: identificador + "-" + peri_codigo + "-" + sem_codigo, per_cedula, tipo_archivo, nombre_archivo }).then(() => {
 
-            removefiles({asig_codigo,peri_codigo,tipo:tipo_archivo,nombre_archivo}).then(()=>{
+            removefiles({ asig_codigo, peri_codigo, tipo: tipo_archivo, nombre_archivo }).then(() => {
 
                 window.location.reload()
 
-            }).catch(()=>{
-                
+            }).catch(() => {
+
                 setError("Error al eliminar el registro del archivo, contacte con el coordinador o intente de nuevo")
 
             })
 
-        }).catch(()=>{
+        }).catch(() => {
 
             setError("Error al eliminar el archivo, contacte con el coordinador o intente de nuevo")
 
         })
 
     }
-    
+
     const DescargarSubmit = () => {
 
-        
+
         var peri_codigo = document.getElementById("peri_codigo").innerText
         var identificador = document.getElementById("identificador").innerText
         var fac_abreviatura = document.getElementById("esquema").innerText.split(".")[0]
@@ -59,23 +60,24 @@ export default function Archivos() {
         var tipo_archivo = document.getElementById("tipo_archivo").innerText
         var nombre_archivo = document.getElementById("archivo_nombre").innerText
         var est_cedula = document.getElementById("est_cedula")
+        var sem_codigo = document.getElementById("sem_codigo")
 
 
         var cedula = per_cedula
 
-        if(est_cedula){
+        if (est_cedula) {
 
             cedula = est_cedula.innerText
         }
 
         setError("")
 
-        descargarArchivo({fac_abreviatura,car_abreviatura,asig_abreviatura:identificador+"-"+peri_codigo,per_cedula:cedula,tipo_archivo,nombre_archivo}).then((url)=>{
+        descargarArchivo({ fac_abreviatura, car_abreviatura, asig_abreviatura: identificador + "-" + peri_codigo + "-" + sem_codigo, per_cedula: cedula, tipo_archivo, nombre_archivo }).then((url) => {
 
             var win = window.open(url, '_blank');
             win.focus();
 
-        }).catch(()=>{
+        }).catch(() => {
 
             setError("Error al descargar el archivo")
 
@@ -104,12 +106,12 @@ export default function Archivos() {
                             </div>
 
                             <div className="col-lg-6 col-md-12 col-sm-12 ">
-                                
+
                                 <p style={{ display: "none" }} id="tipo_archivo" className="modal-tipo_archivo"></p>
-                                <p style={{ display: "none" }} id="archivo_nombre" className="modal-archivo_nombre"></p>        
+                                <p style={{ display: "none" }} id="archivo_nombre" className="modal-archivo_nombre"></p>
 
                                 {error && <strong>{error}</strong>}
-                                <button type="button" className="btn btn-block btn-success " onClick={() => DescargarSubmit()} >Descargar</button>  
+                                <button type="button" className="btn btn-block btn-success " onClick={() => DescargarSubmit()} >Descargar</button>
                                 <button type="button" id="btn_eliminar_archivo" onClick={() => EliminarSubmit()} className="btn btn-block btn-danger">Eliminar</button>
 
                             </div>
